1862a29c27edc9079a592e26e59804097dc67d60
hideaki10 <senshikou1985@gmail.com>
2022-01-13 14:39:18 +0900

    change version to v0.9.8

--- go.mod
+++ go.mod
@@ -3,9 +3,11 @@ module github.com/hideaki10/command-line
 go 1.16
 
 require (
+	github.com/blang/semver v3.5.1+incompatible
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/onsi/ginkgo v1.16.5
 	github.com/onsi/gomega v1.17.0
+	github.com/rhysd/go-github-selfupdate v1.2.3
 	github.com/spf13/cobra v1.3.0
 	github.com/spf13/viper v1.10.0
 )
--- main.go
+++ main.go
@@ -2,8 +2,15 @@ package main
 
 import (
 	"fmt"
+	"log"
 
+	"github.com/blang/semver"
 	"github.com/hideaki10/command-line/cmd"
+	"github.com/rhysd/go-github-selfupdate/selfupdate"
+)
+
+const (
+	version = "0.9.8"
 )
 
 var (
@@ -12,6 +19,15 @@ var (
 )
 
 func main() {
+	fmt.Println("version : " + version)
+	v := semver.MustParse(version[1:])
+	latest, err := selfupdate.UpdateSelf(v, "hideaki10/command-line")
+	if err != nil {
+		log.Fatalf("Binary update failed: %v", err)
+		return
+	} else {
+		fmt.Println("Current version is : " + latest.Version.String())
+	}
 	if GitTag != "" {
 		fmt.Printf("Git tag : %s\nBuilt at: %s\n\n", GitTag, Timestamp)
 	}
